59c57635b1e8d71780e7bd48b93736e59232fd96
Emmanouela Vagianou <emmanouelavagianou58@gmail.com>
2025-03-07 14:02:28 +0200

    bubble van version 1.0.0

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $number = htmlspecialchars($_POST['number']);
+    $message = htmlspecialchars($_POST['text']);
+
+    $to = "hello@bubblevan.gr";
+    $subject = "Μήνυμα από το bubblevan.gr";
+    $body = "Όνομα: $name\nEmail: $email\nΑριθμός: $number\n\nΜήνυμα:\n$message";
+    $headers = "Από: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Το Email στάλθηκε με επιτυχία!";
+    } else {
+        echo "Αποτυχία Αποστολής.";
+    }
+} else {
+    echo "Invalid request.";
+}
+?>
