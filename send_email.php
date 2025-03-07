d193bf7b5b6d5722c1d3bdb21f65d6470ed685cf
Emmanouela Vagianou <emmanouelavagianou58@gmail.com>
2025-03-07 15:03:09 +0200

    fix greek characters

--- send_email.php
+++ send_email.php
@@ -6,9 +6,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = htmlspecialchars($_POST['text']);
 
     $to = "hello@bubblevan.gr";
-    $subject = "Μήνυμα από το bubblevan.gr";
-    $body = "Όνομα: $name\nEmail: $email\nΑριθμός: $number\n\nΜήνυμα:\n$message";
-    $headers = "Από: $email";
+    $subject = "=?UTF-8?B?" . base64_encode("Μήνυμα από το bubblevan.gr") . "?=";
+    $body = "<html><body>";
+    $body .= "<p><strong>Όνομα:</strong> $name</p>";
+    $body .= "<p><strong>Email:</strong> $email</p>";
+    $body .= "<p><strong>Αριθμός:</strong> $number</p>";
+    $body .= "<p><strong>Μήνυμα:</strong><br>" . nl2br($message) . "</p>";
+    $body .= "</body></html>";
+    $headers = "From: $email\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
 
